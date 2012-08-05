46ac30af46bd3e51ee0679bdad9b9a9f1d2bd03d
Dean Sofer <proloser@hotmail.com>
2012-08-05 02:28:24 -0700

    Added google maps directive demo fixes #9

--- js/app.js
+++ js/app.js
@@ -80,6 +80,39 @@ function AnimateCtrl($scope) {
   $scope.items = [];
 }
 
+function MapCtrl($scope) {
+  $scope.myMarkers = [];
+
+  $scope.mapOptions = {
+    center: new google.maps.LatLng(35.784, -78.670),
+    zoom: 15,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  }; 
+
+  $scope.addMarker = function($event) {
+    $scope.myMarkers.push(new google.maps.Marker({
+      map: $scope.myMap,
+      position: $event.latLng
+      }));
+    };
+
+    $scope.setZoomMessage = function(zoom) {
+      $scope.zoomMessage = 'You just zoomed to '+zoom+'!';
+      console.log(zoom,'zoomed')
+    };
+
+    $scope.openMarkerInfo = function(marker) {
+      $scope.currentMarker = marker;
+      $scope.currentMarkerLat = marker.getPosition().lat();
+      $scope.currentMarkerLng = marker.getPosition().lng();
+      $scope.myInfoWindow.open($scope.myMap, marker);
+    };
+
+    $scope.setMarkerPosition = function(marker, lat, lng) {
+      marker.setPosition(new google.maps.LatLng(lat, lng));
+    };
+}
+
 function CurrencyCtrl($scope) {
 	$scope.nums =  {
 		pos : 1000,
